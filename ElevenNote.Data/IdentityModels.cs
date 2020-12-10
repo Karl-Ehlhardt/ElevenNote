6f1745b82f19fd08b6632945d796a620907d790d
Karl Ehlhardt <ehlhardt_karl@gmail.com>
2020-12-10 15:43:18 -0500

    All

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -19,6 +19,14 @@ namespace ElevenNote.Data
             // Add custom user claims here
             return userIdentity;
         }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -33,6 +41,7 @@ namespace ElevenNote.Data
             return new ApplicationDbContext();
         }
         public DbSet<Note> Notes { get; set; }
+        public DbSet<Category> Categorys { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,87 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        public bool CreateCatagory(CategoryCreate model)//post
+        {
+            var entity =
+                new Category()
+                {
+                    Name = model.Name
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categorys.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<CategoryListItem> GetCategorys()//get
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Categorys.Select(
+                            e =>
+                                new CategoryListItem
+                                {
+                                    Id = e.Id,
+                                    Name = e.Name
+                                });
+
+                return query.ToArray();
+            }
+        }
+        public CategoryDetail GetCategoryById(int id)//get
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categorys
+                        .Single(e => e.Id == id);
+                return
+                    new CategoryDetail
+                    {
+                        Id = entity.Id,
+                        Name = entity.Name,
+                    };
+            }
+        }
+        public bool UpdateCategory(CategoryEdit model)//put
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categorys
+                        .Single(e => e.Id == model.Id);
+
+                entity.Id = model.Id;
+                entity.Name = model.Name;
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeleteCategory(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categorys
+                        .Single(e => e.Id == categoryId);
+
+                ctx.Categorys.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
